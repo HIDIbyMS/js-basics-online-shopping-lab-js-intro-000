12302837094911e1aae8916f601377f1d078be40
Michelle Sharpe <hidibyms@gmail.com>
2019-10-26 18:39:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -55,8 +55,13 @@ function total() {
 
 
 //               ${getCart()[0].itemName}
-  function removeFromCart(item){
-    var itemName = "";
+function removeFromCart(item){
+var cart = [];
+cart.push(Object({ itemName: item, itemPrice: price }));
+var price = Math.floor((Math.random() * 100) + 1);
+var itemPrice = price;
+var itemName = item;
+
   for (var i = 0; i < cart.length; i++) {
     if (itemName === cart.length[i]) {
       cart.splice(i+1, 1);
