433bdad803fbf24d318d0e1c71032baa3f84ceac
Michelle Sharpe <hidibyms@gmail.com>
2019-10-26 18:31:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,11 +57,11 @@ function total() {
 //               ${getCart()[0].itemName}
   function removeFromCart(item){
   for (var i = 0; i < cart.length; i++) {
-    if (cart.itemName === cart.length[i]) {
+    if (itemName === cart.length[i]) {
       cart.splice(i+1, 1);
       return cart;
     } else {  
-      (cart.itemName !== cart.length[i]);
+      (itemName !== cart.length[i]);
       return "That item is not in your cart.";
     }
   } 
